7d1a9d9076dc092228087603c8cfda0e99220ce2
willy836 <wilsonkimiri836@gmail.com>
2023-05-02 15:15:23 +0300

    Creat a rails API for back-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: 'Hello, World!')
+Greeting.create(message: 'Hi, there!')
+Greeting.create(message: 'Hello, developers')
+Greeting.create(message: 'Good morning!')
+Greeting.create(message: 'Hello, friends!')
